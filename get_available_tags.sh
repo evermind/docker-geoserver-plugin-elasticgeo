b6f3bdcf43302388b2c90339be3accea2e749bd4
Michael Wyraz <michael.wyraz@evermind.de>
2019-01-09 15:00:38 +0100

    Filter out versions <2.11 which have a different naming scheme

--- get_available_tags.sh
+++ get_available_tags.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TAGS=$( curl -s "https://api.github.com/repos/ngageoint/elasticgeo/releases" | sed -rn 's/.*"tag_name": "(.*)".*/\1/p' | sort -n | uniq | grep -v '^2\..\.' )
+TAGS=$( curl -s "https://api.github.com/repos/ngageoint/elasticgeo/releases" | sed -rn 's/.*"tag_name": "(.*)".*/\1/p' | sort -n | uniq | grep -v '^2\..\.' | grep -v '^2\.10\.' )
 if [ -z "${TAGS}" ]; then
 	echo "No releases found at https://github.com/ngageoint/elasticgeo/releases - check the website and the script $0" >&2
 	exit 1
